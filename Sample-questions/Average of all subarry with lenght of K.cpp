9fda3a773ca316bb34aa82364548724c34303d0d
MFotouhigit config --global user.email mfotouhi@uw.edugit config --global user.name MFotouhi <mfotouhi@uw.edu>
2019-07-11 16:13:24 -0700

    Average of all subarray solution

---
+++ Sample-questions/Average of all subarry with lenght of K.cpp
@@ -0,0 +1,54 @@
+/*Given an array, find the average of all subarrays of size ‘K’ in it.*/
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class FindTheAverageOfSubarray2 {
+public:
+	static vector<double> FindTheAverage(int K, vector<int>& arr) {
+		vector<double> result;
+
+		double windowing=0;
+		for (int i = 0; i < K; i++) {
+			//Summing K elements up
+			windowing += arr[i];
+		}
+		result.push_back(windowing / K);
+
+		for (int j = 0; j < arr.size() - K; j++) {
+			windowing -= arr[j];
+			windowing += arr[j + K];
+			result.push_back(windowing / K);
+		}
+
+		return result;
+	}
+};
+class FindTheAverageOfSubarray {//brute forth method
+public:
+	static vector<double> FindTheAverage(int K, vector<int>& arr) {
+		vector<double> result;
+
+		for (int i = 0; i <= arr.size() - K; i++) {
+			//Summing K elements up
+			double sum = 0;
+			for (int j = i; j < i + K; j++)  sum += arr[j];
+			result.push_back(sum / K); // Calculated 
+		}
+		return result;
+	}
+};
+//--------------------------------------------------------------------------------------//
+int main() {
+	vector <int> arr = { 1, 3, 2, 6, -1, 4, 1, 8, 2 };
+	int K = 5;
+
+	vector <double> ans = FindTheAverageOfSubarray2::FindTheAverage(K, arr);
+	cout << "\n\tThe Averages are: ";
+	for (auto a : ans)
+		cout << a << " ";
+	cout << "\n\n\t";
+
+	system("pause");
+	return 0;
+}
