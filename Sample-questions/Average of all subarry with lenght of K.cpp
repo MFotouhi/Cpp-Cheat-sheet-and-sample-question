e8020b701c796211cc61b4add7a8afefdb7cefed
MFotouhigit config --global user.email mfotouhi@uw.edugit config --global user.name MFotouhi <mfotouhi@uw.edu>
2019-07-11 16:17:25 -0700

    commit

--- Sample-questions/Average of all subarry with lenght of K.cpp
+++ Sample-questions/Average of all subarry with lenght of K.cpp
@@ -3,32 +3,29 @@
 #include<vector>
 using namespace std;
 
-class FindTheAverageOfSubarray2 {
+class FindTheAverageOfSubarray2 { //Sliding Window sum: O(N)
 public:
 	static vector<double> FindTheAverage(int K, vector<int>& arr) {
 		vector<double> result;
-
 		double windowing=0;
 		for (int i = 0; i < K; i++) {
 			//Summing K elements up
 			windowing += arr[i];
 		}
 		result.push_back(windowing / K);
-
+		//Windowing
 		for (int j = 0; j < arr.size() - K; j++) {
 			windowing -= arr[j];
 			windowing += arr[j + K];
 			result.push_back(windowing / K);
 		}
-
 		return result;
 	}
 };
-class FindTheAverageOfSubarray {//brute forth method
+class FindTheAverageOfSubarray {//Brute forth method O(N*K)
 public:
 	static vector<double> FindTheAverage(int K, vector<int>& arr) {
 		vector<double> result;
-
 		for (int i = 0; i <= arr.size() - K; i++) {
 			//Summing K elements up
 			double sum = 0;
